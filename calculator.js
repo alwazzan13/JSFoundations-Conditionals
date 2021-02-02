304e0cd42b4546fed5873e7da3b5646c28fd865e
alwazzan13 <f.alwazzan1@gmail.com>
2021-02-02 18:49:57 +0300

    solved the conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 // Define the numbers and the operation here
-
+let num1 = 9
+let num2 = 11
+let str1 = "+"
 // Write if-statements here
+if (str1 === "+") {
+   console.log(num1 + num2);
+}
+else {
+    console.log("FALSE");
+}
